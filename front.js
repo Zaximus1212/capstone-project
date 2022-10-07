c209e512c904e935b201928ad551da98936e5742
Zachary Boughton <zboughton@yahoo.com>
2022-10-06 20:03:57 -0600

    putting in most queryselectors

--- front.js
+++ front.js
@@ -1,4 +1,44 @@
+const form = document.querySelector('form')
+const nameInput = document.querySelector('#name-input')
+const skills = document.querySelector('#skills')
+const moneyManagement = document.querySelector('#money-management')
+const appointments = document.querySelector('#appointments')
+const communityIntegration = document.querySelector('#community-integration')
+const preffered = document.querySelector('#preffered')
+const interaction = document.querySelector('#interaction')
+const activities = document.querySelector('#activities')
+const food = document.querySelector('#food')
+const mealPlan = document.querySelector('#meal-plan')
+const behaviors = document.querySelector('#behaviors')
+const comments = document.querySelector('#comments')
 const note = document.querySelector("#note")
+
+
+function handleSubmit(e) {
+    e.preventDefault()
+
+    if (nameInput.value < 1) {
+        alert ('You must enter a button name')
+        return
+    }
+
+    let type = document.querySelector('input[name="type-button"]:checked').value
+    let body = {
+        name: nameInput.value, 
+        type: type, 
+        buttonId: +buttonId,
+
+    }
+
+    axios.post('http://localhost:4004/cities', body)
+        .then(() => {
+            countrySelect.value = 1
+            nameInput.value = ''
+            document.querySelector('#rating-one').checked = true
+            getCities()
+        })
+}
+
 
 
 const writerPeriod = arr => {
@@ -37,11 +77,11 @@ const allInOneSpace = (str1, str2) => {
 
 
 
-let skills = ''
+let skill = ''
 const runner = skillText =>{
-    skills = allInOnePeriod(skills, skillText)
-    console.log(skills)
-    return skills
+    skill = allInOnePeriod(skill, skillText)
+    console.log(skill)
+    return skill
 }
 runner("1")
 runner("2")
@@ -52,18 +92,29 @@ note.innerHTML = `<h1>Your note starts here</h1>
 <p>
 <br>
 <br>
-${skills} <br><br>
-${skills} <br><br>
-${skills} <br><br>
+${skill} <br><br>
+${skill} <br><br>
+${skill} <br><br>
 
 </p>`
+
+
+
+
+
+
+
+
+
 // ${money}<br><br>
 // ${appointments}<br><br>
 // ${integration}<br><br>
-// ${oreffered}<br><br>
+// ${preffered}<br><br>
 // ${interaction}<br><br>
 // ${activities}<br><br>
 // ${food}<br><br>
 // ${mealPlan}<br><br>
 // ${behaviors}<br><br>
 // ${comment}<br><br>
+
+form.addEventListener('submit', handleSubmit)
